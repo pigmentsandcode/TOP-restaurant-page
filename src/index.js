a8d6bbbb5721d1e681d194813263051690e1d237
Heather H. <196852197+pigmentsandcode@users.noreply.github.com>
2025-04-16 16:03:39 -0500

    Style navbar

--- src/index.js
+++ src/index.js
@@ -9,16 +9,24 @@ const contentDiv = document.querySelector("#content");
 const homeBtn = document.querySelector("#home-btn");
 const menuBtn = document.querySelector("#menu-btn");
 const aboutBtn = document.querySelector("#about-btn");
+let activeBtn = homeBtn;
+activeBtn.classList.add("active");
 
 function handleNavClick(e) {
   contentDiv.removeChild(contentDiv.firstChild);
+
+  activeBtn.classList.remove("active");
   if (e.target.id === "home-btn") {
     contentDiv.append(populateHome());
+    homeBtn.classList.add("active");
   } else if (e.target.id === "menu-btn") {
     contentDiv.append(populateMenu());
+    menuBtn.classList.add("active");
   } else {
     contentDiv.append(populateAbout());
+    aboutBtn.classList.add("active");
   }
+  activeBtn = e.target;
 }
 
 homeBtn.addEventListener("click", handleNavClick);
